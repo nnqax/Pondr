722acd7332293a852fa1b4c2a8b1d48d2f6bb8ca
rahiqayman-wq <rahiqayman@gmail.com>
2026-07-30 17:19:20 +0300

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,1 +1,30 @@
 
+// ===============================
+// Local Storage Helper
+// ===============================
+
+// Save any data
+function saveData(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
+}
+
+// Load data
+function loadData(key) {
+    const data = localStorage.getItem(key);
+
+    if (data) {
+        return JSON.parse(data);
+    }
+
+    return null;
+}
+
+// Remove data
+function removeData(key) {
+    localStorage.removeItem(key);
+}
+
+// Clear everything
+function clearAllData() {
+    localStorage.clear();
+}
